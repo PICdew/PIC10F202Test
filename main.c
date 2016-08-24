7d0d68ba9bab0988a593675934aa8f0157c41fc2
Simon Wyss <wysi94@gmail.com>
2016-08-24 21:55:53 +0200

    first main.c

---
+++ main.c
@@ -0,0 +1,31 @@
+/*
+PIC10F202Test - A test programm fo the PIC10F202 microcontroller
+Copyright (C) 2016-2016 Simon Wyss <wysi94@gmail.com>
+
+This file is part of PIC10F202Test.
+
+PIC10F202Test is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+PIC10F202Test is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the Lesser
+GNU General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with PIC10F202Test.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
