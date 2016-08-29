74293069269df0fe60a35acb3cc9655b6e74992d
Simon Wyss <wysi94@gmail.com>
2016-08-29 15:28:03 +0200

    pwm fan control works with hex rotary switch as input

--- main.c
+++ main.c
@@ -29,20 +29,26 @@ along with PIC10F202Test.  If not, see <http://www.gnu.org/licenses/>.
 
 //#include <10F202.h> 
 #include <xc.h>
+#include <stdint.h>
 
 #define _XTAL_FREQ 4000000
 
-//#pragma FOSC4_bit FC
-
-//asm( " org     0x000 " );
-//asm( " movwf   0xFC " );
-
-//defines
+//defines for OPTION register
 #define nCHANWAKE	7  //nGPWU
 #define nWEAKPULL	6	//GPPU
 #define T0CKIPIN	5	//T0CS
 #define T0NEGEDGE	4	//T0SE
 #define PRESCALWDT  3  //PSA
+
+//Pin-Out Hex Rotary Encoder/Switch
+/* HEX-Switch	MCU
+ * C			GND
+ * 1			not connected (because GP0 is already used for PWM)
+ * 2			GP1
+ * 4			GP2
+ * 8			GP3
+ *  
+ */
 
 void main(void)
 {
@@ -50,7 +56,7 @@ void main(void)
     asm("MOVWF OSCCAL");
     
 	//GP0, GP1, GP2, GP*/MCLR
-    TRIS = 0; //set All Pins as Output, TRIS = TRISGPIO
+    TRIS = 0b1110; //Inputs = 1, GP0 = Output, TRIS = TRISGPIO
 	//ENABLE: weak-pullups, Timer0 intosc source, Prescaler for WDT - no Prescaler for Timer0
 	OPTION = (1<<nCHANWAKE)|(0<<nWEAKPULL)|(0<<T0CKIPIN)|(0<<T0NEGEDGE)|(1<<PRESCALWDT); 
 	
@@ -67,36 +73,224 @@ void main(void)
 	
 	//8-Bit Timer init:
 	GP0 = 0;
-#define pvalue 20
-#define ptotal 40
+//#define pvalue 20
+
+#define pmax 9
+
+	uint8_t pwmValue = 1;
+	
+	uint8_t switchBuffer = 0;
+	uint8_t switchValueOld = 0;
+	
+	switchValueOld = GPIO & 0b1110; //delet GP0, because this is no input
+	switchValueOld = switchValueOld >> 1; //LSB right justified
+	
+	//int i = 0;
+	uint8_t switchTimer = 0;
 	
 	//main loop
 	while(1){
-       /*
-		GP0 = 1;
-       __delay_ms(20);
-       GP0 = 0;
-       __delay_ms(20);
-	   */
-		
-		
-		GP2 = 1; //probably use HIGH-Impendance mode instead of PULL-HIGH: TRIS = 0b0001;
-		//TRIS = 0b0001; //works, but rising edge is not steep because of the weak-pullup
-		__delay_us(pvalue);
 		/*
-		int i = 0; //Total 40 cycles for PWM
-		while(i<10){
-			i++; //one increment = 1 instruction -> 10 = 10us
+		switchBuffer = GPIO & 0b1110; //delet GP0, because this is no input
+		switchBuffer = switchBuffer >> 1; //LSB right justified
+		
+		if(switchBuffer > switchValueOld){ //rotation to the right, higher value
+			if(pwmValue < pmax){
+				pwmValue++;
+			}
+		}
+		else if(switchBuffer < switchValueOld){ //rotation to the left, lower value
+			if(pwmValue > 0){
+				pwmValue--;
+			}
 		}
 		*/
-		GP2 = 0;  //TRISGIPIO = 0b0000;
-		//TRIS = 0b0000;
-		__delay_us(ptotal-pvalue);
+		//pwmValue = (GPIO  & 0b00001110) >> 1;
+		pwmValue = ((GPIO ^ 0xFF) & 0b00001110) >> 1;
+		
+#define ptotal 40
+#define p0 0
+#define p1 5
+#define p2 12 
+#define p3 20
+#define p4 25
+#define p5 30
+#define p6 35
+#define p7 40
+
+#define p8 35
+#define p9 40
+		
+		//switchTimer = 0;
+		switch(pwmValue){
+			case 0:
+				while(1){ 
+					GP0 = 0;
+					switchTimer++;
+					if(switchTimer == 0){
+						break;
+					}
+					//__delay_us(p0);
+					//GP0 = 0;
+					__delay_us(ptotal-p0);
+				}
+				break;
+			case 1:
+				while(1){
+					GP0 = 1;
+					switchTimer++;
+					if(switchTimer == 0){
+						break;
+					}
+					__delay_us(p1);
+					GP0 = 0;
+					__delay_us(ptotal-p1);
+				}
+				break;
+			case 2:
+				while(1){
+					GP0 = 1;
+					switchTimer++;
+					if(switchTimer == 0){
+						break;
+					}
+					__delay_us(p2);
+					GP0 = 0;
+					__delay_us(ptotal-p2);
+				}
+				break;
+			case 3:
+				while(1){
+					GP0 = 1;
+					switchTimer++;
+					if(switchTimer == 0){
+						break;
+					}
+					__delay_us(p3);
+					GP0 = 0;
+					__delay_us(ptotal-p3);
+				}
+				break;
+			case 4:
+				while(1){
+					GP0 = 1;
+					switchTimer++;
+					if(switchTimer == 0){
+						break;
+					}
+					__delay_us(p4);
+					GP0 = 0;
+					__delay_us(ptotal-p4);
+				}
+				break;
+			case 5:
+				while(1){
+					GP0 = 1;
+					switchTimer++;
+					if(switchTimer == 0){
+						break;
+					}
+					__delay_us(p5);
+					GP0 = 0;
+					__delay_us(ptotal-p5);
+				}
+				break;
+			case 6:
+				while(1){
+					GP0 = 1;
+					switchTimer++;
+					if(switchTimer == 0){
+						break;
+					}
+					__delay_us(p6);
+					GP0 = 0;
+					__delay_us(ptotal-p6);
+				}
+				break;
+			case 7:
+				while(1){
+					GP0 = 1;
+					switchTimer++;
+					if(switchTimer == 0){
+						break;
+					}
+					__delay_us(p7);
+					//GP0 = 0;
+					__delay_us(ptotal-p7);
+				}
+				break;
+			case 8:
+				while(1){
+					GP0 = 1;
+					switchTimer++;
+					if(switchTimer == 0){
+						break;
+					}
+					__delay_us(p8);
+					GP0 = 0;
+					__delay_us(ptotal-p8);
+				}
+				break;
+			case 9:
+				while(1){
+					GP0 = 1;
+					switchTimer++;
+					if(switchTimer == 0){
+						break;
+					}
+					__delay_us(p9);
+					//GP0 = 0;
+					//__delay_us(ptotal-p9);
+				}
+				break;
+			default:
+				break;
+		}
+		
+		/*
+		GP0 = 1;
+		for(i=0;i<pwmValue;i++){
+			__delay_us(1);
+		}
+		GP0 = 0;
+		for(i=0;i<(pmax-pwmValue);i++){
+			__delay_us(1);
+		}
+		*/		
+		
+		//GP0 = 1;
+		/*
+		int i;
+		for(i=0;i<pwmValue;i++){
+			//asm("nop");
+		}
+		*/
+		/*int i = 0;
+		while(i<pwmValue){
+			i++;
+		}*/
+		
+		//GP0 = 0;
+		/*
+		for(i=0;i<(ptotal - pwmValue);i++){
+			//asm("nop");
+		}
+		*/
+		
 		/*
 		i = 0;
-		while(i<30){
+		while(i<pwmValue){
 			i++;
 		}
+		*/
+				
+		/*
+		GP0 = 1; //probably use HIGH-Impendance mode instead of PULL-HIGH: TRIS = 0b0001;
+		//TRIS = 0b0001; //works, but rising edge is not steep because of the weak-pullup
+		__delay_us(pvalue);
+
+		GP0 = 0;  //TRISGIPIO = 0b0000;
+		__delay_us(ptotal-pvalue);
 		*/
     }
     
